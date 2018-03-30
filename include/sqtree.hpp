365203ac73743dc07f5dabe9f9497299a4b83d81
dvansa <daniel.vansa@gmail.com>
2018-03-30 15:05:40 +0200

    Straightforward insert() search()

--- include/sqtree.hpp
+++ include/sqtree.hpp
@@ -1,6 +1,11 @@
 #ifndef SQTREE_HPP
 #define SQTREE_HPP
 
+#include <array>
+#include <list>
+#include <vector>
+
+//AABB Axis aligned rectangle
 template <typename Scalar>
 class AABB
 {
@@ -24,14 +29,113 @@ public:
 	Scalar y2;
 };
 
+//Node
+template <typename Scalar, class Object, std::size_t MAX_ITEMS = 16>
+class Node : public AABB<Scalar>
+{
+public:
+	typedef AABB<Scalar> AABBType;
+	typedef std::pair<AABBType, Object*> ObjectListType;
+
+	//TopLeft TopRight BottomLeft BottomRight
+	enum
+	{
+		TL, TR, BL, BR
+	};
+
+
+	Node(Node * parent, AABB<Scalar> rect) : _parent(parent), AABB<Scalar>(rect)
+	{
+		_child[0] = _child[1] = _child[2] = _child[3] = nullptr;
+	}
+
+	void insert(const AABBType & objrect, Object * newobj)
+	{
+		Scalar halfs = (Scalar)0.5*(x2 - x1);
+		if (_objects.empty())
+		{
+			_objects.push_front(ObjectListType(objrect, newobj));
+		}
+		else if (AABBType(x1, y1, x1 + halfs, y1 + halfs).contains(objrect))
+		{
+			if (_child[TL] == nullptr)
+				_child[TL] = new Node<Scalar, Object, MAX_ITEMS>(this, AABBType(x1, y1, x1 + halfs, y1 + halfs));
+			_child[TL]->insert(objrect, newobj);
+		}
+		else if (AABBType(x1 + halfs, y1, x2, y1 + halfs).contains(objrect))
+		{
+			if (_child[TR] == nullptr)
+				_child[TR] = new Node<Scalar, Object, MAX_ITEMS>(this, AABBType(x1 + halfs, y1, x2, y1 + halfs));
+			_child[TR]->insert(objrect, newobj);
+		}
+		else if (AABBType(x1, y1 + halfs, x1 + halfs, y2).contains(objrect))
+		{
+			if (_child[BL] == nullptr)
+				_child[BL] = new Node<Scalar, Object, MAX_ITEMS>(this, AABBType(x1, y1 + halfs, x1 + halfs, y2));
+			_child[BL]->insert(objrect, newobj);
+		}
+		else if (AABBType(x1 + halfs, y1 + halfs, x2, y2).contains(objrect))
+		{
+			if (_child[BR] == nullptr)
+				_child[BR] = new Node<Scalar, Object, MAX_ITEMS>(this, AABBType(x1 + halfs, y1 + halfs, x2, y2));
+			_child[BR]->insert(objrect, newobj);
+		}
+		//Not contained on childs
+		else
+		{
+			_objects.push_front(ObjectListType(objrect, newobj));
+		}
+	}
+
+	void search(const AABBType & query, std::vector<Object*> & found) const
+	{
+		if (intersects(query))
+		{
+			for (auto it = _objects.begin(); it != _objects.end(); ++it)
+			{
+				if (it->first.intersects(query))
+				{
+					found.push_back(it->second);
+				}
+			}
+		}
+
+		for (auto i = 0; i < 4; ++i)
+			if (_child[i] != nullptr)
+				_child[i]->search(query, found);
+	}
+
+	Node * _parent;
+	std::array<Node *, 4> _child;
+
+	std::list<ObjectListType > _objects;
+};
+
 template <typename Scalar, class Object>
 class SQTree
 {
 public:
-	typedef AABB<Scalar> Box;
 
-	SQTree()
-	{}
+	typedef Node<Scalar, Object> NodeType;
+	typedef AABB<Scalar> AABBType;
+
+	SQTree(Scalar size)
+	{
+		_root = new NodeType(nullptr, AABBType(0,0,size,size));
+	}
+
+	void insert(const AABBType & objrect, Object * newobj)
+	{
+		if (_root->contains(objrect))
+			_root->insert(objrect, newobj);
+	}
+
+	void search(const AABBType & query, std::vector<Object*> & found) const
+	{
+		_root->search(query, found);
+	}
+
+	NodeType * _root;
 };
 
 #endif
--- src/sqtree.cpp
+++ src/sqtree.cpp
@@ -1,6 +1,8 @@
 #include <sqtree.hpp>
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -18,5 +20,23 @@ int main(int argc, char ** argv)
 	cout << b1.intersects(b2) << " - " << b1.contains(b2) << endl;
 	cout << b1.intersects(b3) << " - " << b1.contains(b3) << endl;
 	cout << b1.intersects(b4) << " - " << b1.contains(b4) << endl;
+
+	SQTree<int, string> tree(1024);
+
+	vector<string> objects = {"obj1","obj2", "obj3"};
+
+	tree.insert(b2, &objects[0]);
+	tree.insert(b3, &objects[1]);
+	tree.insert(b4, &objects[2]);
+
+	vector<string *> found;
+
+	tree.search(AABB<int>(8,8,20,20),found);
+
+	cout << "Query" << endl;
+	for (auto v : found)
+		cout << "\t " << *v << endl;
+
+
 	return 0;
 }
